39fa168968c26d73c219b02bb14d924086e6331b
Bitcoin Buble <bitcoin.buble@gmail.com>
2018-11-27 09:47:38 +0000

    add reverse martingale algo

---
+++ reverse-martingale.js
@@ -0,0 +1,62 @@
+/*********************************************/
+
+const gamesUntilBankrupt = 5000;
+const baseCashOutPercent = 120;
+
+/*********************************************/
+
+let cashOutPercent = cashOutPercent;
+const username = engine.getUsername();
+
+let bet = getOptimalBaseBet(gamesUntilBankrupt);
+
+let totalStake = 0;
+let totalBonus = 0;
+
+function getOptimalBaseBet(gamesUntilBankrupt) {
+  const balance = engine.getBalance();
+  let baseBet = balance / gamesUntilBankrupt;
+
+  const roundedBet = Math.floor(baseBet / 100) * 100;
+
+  console.log(
+    `Optimal base bet for ${gamesUntilBankrupt} games till bankrupt... ${Math.floor(
+      roundedBet
+    )}`
+  );
+
+  return roundedBet > 100 ? roundedBet : 100;
+}
+
+const handleStart = info => {
+  console.log("Game Starting in " + info.time_till_start);
+  console.log(`Placing bet ${bet}`);
+  engine.placeBet(bet, cashOutPercent);
+  totalStake += bet;
+};
+
+const handleCrash = data => {
+  console.log("Game crashed at ", data.game_crash);
+  console.log(data);
+  cashOutPercent += 100;
+  if (data.game_crash >= cashOutPercent) {
+    bet = getOptimalBaseBet(gamesUntilBankrupt);
+    cashOutPercent = baseCashOutPercent;
+  }
+
+  if (data.bonuses && data.bonuses[username]) {
+    totalBonus += data.bonuses[username];
+  }
+
+  console.log("Total Stake:  ", totalStake);
+  console.log("Total Bonus:  ", totalBonus);
+  console.log("Percent Edge: ", (100 * totalBonus) / totalStake);
+};
+
+engine.on("game_starting", handleStart);
+
+engine.on("game_started", function(data) {
+  console.log("Game Started", data);
+});
+
+engine.on("game_crash", handleCrash);
